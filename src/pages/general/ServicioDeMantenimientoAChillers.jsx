f69f9056d1ee11851466713d980ac5968a0c4d91
MaicolDevJr <michael.pina@holdingvml.net>
2025-10-29 00:40:04 -0500

    Se corrigieron errores de sintaxis en los componentes de servicios de aire acondicionado, asegurando que las clases CSS y los atributos estén correctamente escritos. Se mejoró la consistencia en la presentación de los beneficios ofrecidos en los componentes relacionados con la reparación y mantenimiento de aires acondicionados.

--- src/pages/general/ServicioDeMantenimientoAChillers.jsx
+++ src/pages/general/ServicioDeMantenimientoAChillers.jsx
@@ -126,7 +126,6 @@ const ServicioDeMantenimientoAChillers = () => {
         </div>
       </div>
      </div>
-   </div>
   );
 };
 
--- src/pages/general/ServicioDeReparacionDeAireAcondicionado.jsx
+++ src/pages/general/ServicioDeReparacionDeAireAcondicionado.jsx
@@ -150,7 +150,6 @@ const ServicioDeReparacionDeAireAcondicionado = () => {
         </div>
       </div>
      </div>
-   </div>
   );
 };
 
